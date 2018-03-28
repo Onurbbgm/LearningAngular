3e0e2462fabce5e15ec340010d2a8a343f9e63ad
Onurbbgm <brunogmoretto@hotmail.com>
2018-03-28 16:20:23 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,36 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+    
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>HELLO GET!</h1></body></html>")
+    
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        self._set_headers()
+        self.wfile.write("<html><body><h1>HELLO POST!</h1><pre>"+post_data+"</pre></body></html>")
+        print post_data
+    
+def run(server_class=HTTPServer, handler_class=Server, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
+    
